fb4d3970bb97f494bbc0b0f94e1a3a2d63be83d9
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-09-25 01:04:50 +0500

    new push

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: "dist", // 👈 Vercel dist ko serve karega
     emptyOutDir: true,
   },
 });
